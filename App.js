e690315b28e94a1c7cf55bb809b7582d88046bba
Manas173 <rockmanas121@gmail.com>
2019-02-02 19:26:25 +0530

    Enter and remove single note

--- App.js
+++ App.js
@@ -1,2 +1,18 @@
+var os=require('os');
 var yargs=require('yargs');
+var fs=require('fs');
+//var _=require('lodash');
+var notes=require('./notes.js');
 
+const command=process.argv[2];
+const argv=yargs.argv;
+var feedback='Unable to access , command fault !';
+if(command==='addNote')
+	feedback=notes.readNote(argv);
+else if(command==='deleteNote')
+	feedback=notes.deleteNote(argv);
+else if(command==='deleteAllNote')
+	feedback=notes.deleteAllNote();
+else if(command==='replaceNote')
+	feedback=notes.replaceNote();
+console.log(feedback);
