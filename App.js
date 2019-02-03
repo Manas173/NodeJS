4f4170b2e3e264bbfb20a2d64891a8b5cf4303a0
Manas173 <rockmanas121@gmail.com>
2019-02-04 03:17:20 +0530

    Updated required files through yargs

--- App.js
+++ App.js
@@ -3,10 +3,27 @@ var yargs=require('yargs');
 var fs=require('fs');
 //var _=require('lodash');
 var notes=require('./notes.js');
-
+var feedback='Unable to access , command fault !!!';
 const command=process.argv[2];
-const argv=yargs.argv;
-var feedback='Unable to access , command fault !';
+const request=require('request');
+const argv=yargs.
+	command('addNote','Add a newnote',{
+		title: {
+			describe:'The title of the user',
+			demand:true
+		},
+		body: {
+			describe:'The body for each title',
+			demand: true
+		}
+	}).help().alias('help','h').argv;
+debugger;
+
+//Request to be done later
+
+var decURI="Hey there I am using Kali";
+var encURI=encodeURIComponent(decURI);
+console.log(encURI);
 if(command==='addNote')
 	feedback=notes.readNote(argv);
 else if(command==='deleteNote')
@@ -15,4 +32,6 @@ else if(command==='deleteAllNote')
 	feedback=notes.deleteAllNote();
 else if(command==='replaceNote')
 	feedback=notes.replaceNote();
+else if(command==='Error')
+	feedback='Please fill all the required fields';
 console.log(feedback);
