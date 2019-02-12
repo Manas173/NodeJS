8e408086538586a0c5a98064d0342f01a089a7c5
Manas173 <rockmanas121@gmail.com>
2019-02-12 07:32:15 +0530

    Using request inside a promise

---
+++ promises2.js
@@ -0,0 +1,29 @@
+var request = require('request');
+var geocodeAddress = (address) => {
+	return new Promise((resolve,reject) => {
+		var encodedURI = encodeURIComponent(address); 
+		request({
+			uri:`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${encodedURI}&key=AIzaSyDMYuu1EyK9oYhjdlnGbSW_iPrh-LRh264`,
+			json:true
+		},
+		(error,response,body)=>{
+			if(response.statusCode!==200 || body.status==='ZERO_RESULTS' || body.results===null)
+				reject('The address has not been found !!!');
+			else
+				resolve({
+					lat:body.results[0].geometry.location.lat,
+					lng:body.results[0].geometry.location.lng
+				})
+		})
+	})
+}
+
+geocodeAddress('835215').then((message)=>{
+	console.log(JSON.stringify(message,undefined,2));
+},(errorMessage)=>{
+	console.log(errorMessage);
+})
+
+module.exports={
+	geocodeAddress
+};
