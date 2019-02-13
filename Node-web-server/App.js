ff352b47d5627bb0236073f0d6442653cae274d1
Manas173 <rockmanas121@gmail.com>
2019-02-13 14:40:49 +0530

    Node web server

---
+++ Node-web-server/App.js
@@ -0,0 +1,59 @@
+const express = require('express');
+var hbs=require('hbs');
+var app=express();	//new express app
+var fs=require('fs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine','hbs');
+
+app.get('/',(req,res)=>{
+	res.send({Name:'Manas',
+			Roll:'BE1017316'});
+})
+
+app.use((req,res,next)=>{
+	var log=`${new Date().toString()} ${req.url} : ${req.method}`;
+	fs.appendFile('server.log',log+'\n',(err)=>{
+		if(err)
+			console.log('Error has occur !!!');
+	});
+	next();
+})
+
+
+app.get('/bad',(req,res)=>{
+	res.send({heading:'Request type',
+			details:'It is a real bad request'});
+})
+
+app.get('/help',(req,res)=>{
+	res.render('../help.hbs',{
+		message: 'welcome to my website'
+	});
+})
+
+hbs.registerHelper('screamIt',(word)=>{
+	return word.toUpperCase();
+})
+
+app.use(express.static(__dirname));
+
+hbs.registerHelper('getCurrentYear',()=>{
+	return new Date().getFullYear();
+})
+
+app.get('/about',(req,res)=>{
+	res.render('About.hbs',{
+		pageTitle: 'About the page',
+		year: new Date().getFullYear()
+	});
+})
+
+
+app.use((req,res,next)=>{
+	res.render('maintanence.hbs');
+})
+
+app.listen((3000),()=>{
+	console.log('Server started at PORT 3000');
+});
