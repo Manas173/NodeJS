5e1b61eb6802be4a7e23436769af4f8a9af8a7df
Manas173 <rockmanas121@gmail.com>
2019-02-20 13:41:32 +0530

    fetc-data-Object-Id

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,4 +1,4 @@
-const MongoClient = require('mongodb').MongoClient;
+const {MongoClient,ObjectId} = require('mongodb');
 
 // MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
 // 	if(err)
@@ -15,27 +15,67 @@ const MongoClient = require('mongodb').MongoClient;
 // 	db.close();		//Disconnect from server
 // });
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
-	if(err)
-		return console.log('Unable to connect !');
-	console.log('Conneted to mongodb !');
-	db.collection('Todos').insertOne({
-		text:'This is a random text',
-		completed: false
-	},(err,result)=>{
-		if(err)
-			return console.log('Unable to insert document to Todos !');
-		console.log(JSON.stringify(result.ops,undefined,2));
-	});
+// MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+// 	if(err)
+// 		return console.log('Unable to connect !');
+// 	console.log('Conneted to mongodb !');
+// 	db.collection('Todos').insertOne({
+// 		text:'This is a random text',
+// 		completed: false
+// 	},(err,result)=>{
+// 		if(err)
+// 			return console.log('Unable to insert document to Todos !');
+		// console.log(JSON.stringify(result.ops,undefined,2));
+	// 	console.log(result.ops);
+	// });
 
-	db.collection('Users').insertOne({
-		name: 'Manas Ranjan Swain',
-		age: 21,
-		location: 'Ranchi'
-	},(err,result)=>{
-		if(err)
-			return console.log('Unable to insert document into Users');
-		console.log(JSON.stringify(result.ops,undefined,2));
+	// db.collection('Users').insertOne({
+	// 	name: 'Manas Ranjan Swain',
+	// 	age: 21,
+	// 	location: 'Ranchi'
+	// },(err,result)=>{
+	// 	if(err)
+	// 		return console.log('Unable to insert document into Users',err);
+		// console.log(JSON.stringify(result.ops,undefined,2));
+		// console.log(JSON.stringify(result.ops,undefined,2));
+// 	})
+// 	db.close();
+// });
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+	db.collection('Users').find({_id: new ObjectId("5c6cffa672a74965dc3ab289")}).toArray().then((result)=>{
+		console.log(result);
+	}).catch((err)=>{
+		console.log('Error has occured!');
+		console.log(err);
 	})
-	db.close();
+
+	db.collection('Users').find({name: 'Ranjan'}).toArray().then((response)=>{
+		console.log(response);
+	}).catch((err)=>{
+		console.log('Error occured',err);
+	})
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
