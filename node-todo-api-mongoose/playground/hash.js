575641cfdd7843894ee519e9e423326999863bfe
Manas173 <rockmanas121@gmail.com>
2019-03-10 23:33:17 +0530

    Hashing using jsonwebtoken library

--- node-todo-api-mongoose/playground/hash.js
+++ node-todo-api-mongoose/playground/hash.js
@@ -1,15 +1,31 @@
-const {SHA256} = require('crypto-js');
-
-var value = 4;
-var hash = SHA256(value).toString();
+const jwt = require('jsonwebtoken');
 
 var data = {
 	id: 4
 };
 
-var token = {
-	data,
-	token : SHA256(JSON.stringify(data)+'itsthesecret').toString()	//Adding SALT
-}
+var token = jwt.sign(data,'itsthesecret');
 
-console.log(token);
+var decode = jwt.verify(token,'itsthesecret');
+console.log(decode);
+
+if(decode.id === data.id)
+	console.log('Verified');
+else
+	console.log('Not Verified Do Not Trust');
+
+// const {SHA256} = require('crypto-js');
+
+// var value = 4;
+// var hash = SHA256(value).toString();
+
+// var data = {
+// 	id: 4
+// };
+
+// var token = {
+// 	data,
+// 	token : SHA256(JSON.stringify(data)+'itsthesecret').toString()	//Adding SALT
+// }
+
+// console.log(token);
