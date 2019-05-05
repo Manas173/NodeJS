508f1de56dca0e0f54b927c43aec621afe5f7f01
Manas173 <rockmanas121@gmail.com>
2019-05-05 12:37:23 +0530

    Chatroom completed

--- chatApp/public/js/index.js
+++ chatApp/public/js/index.js
@@ -1,9 +1,15 @@
 var socket = io();
 socket.on('connect',function () {
-	console.log('Connected to server !')
+    console.log('Connected to server !')
+    var li= jQuery('<li> </li>');
+    li.text('New user joined the chatroom !');
+    jQuery(`#messages`).append(li);
 })
 socket.on('disconnect',function() {
-	console.log('Disconnected from server !')
+    console.log('Disconnected from server !')
+    var li= jQuery('<li> </li>');
+    li.text('User disconnected from the chatroom !');
+    jQuery(`#messages`).append(li);
 })
 socket.on('newMail',function(email) {
     console.log(email)
