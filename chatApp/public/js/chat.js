00b06da5ac60dd9e4bead0a269d9767aa4a4024e
Manas173 <rockmanas121@gmail.com>
2019-05-08 11:53:10 +0530

    Adding join Page

--- chatApp/public/js/chat.js
+++ chatApp/public/js/chat.js
@@ -1,6 +1,7 @@
 var socket = io();
 socket.on('connect',function () {
     console.log('Connected to server !')
+    var params = jQuery.deparam(window.location.search)
 })
 socket.on('disconnect',function() {
     console.log('Disconnected from server !')
