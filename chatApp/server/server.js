1654dbde2d0a34ed4f96a5361a857ff5712b3c77
Manas173 <rockmanas121@gmail.com>
2019-05-08 15:39:22 +0530

    User name display

--- chatApp/server/server.js
+++ chatApp/server/server.js
@@ -8,6 +8,7 @@ var http = require('http');
 var server = http.createServer(app);
 var io = socketIO(server);
 var moment = require('moment');
+const {isRealString} = require('./utils/validation.js');
 var {generateMessage,generateLocationMessage} = require('./utils/message');
 
 app.use(express.static(publicPath));
@@ -15,17 +16,34 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) => {
 	console.log('New user connected !');
 
-	socket.emit('newMessage',{
-		from: 'Admin',
-		text: 'Welcome to the chat App',
-		createdAt: moment().valueOf()
+	// socket.emit('newMessage',{
+	// 	from: 'Admin',
+	// 	text: 'Welcome to the chat App',
+	// 	createdAt: moment().valueOf()
+	// })
+
+	socket.on('join',(params,callback) => {
+		if(!isRealString(params.name)|| !isRealString(params.room)){
+			callback('Username and roomname are required !');
+		}
+		socket.emit('newMessage',{
+			from: 'Admin',
+			text: 'Welcome to the chat App',
+			createdAt: moment().valueOf()
+		})
+		socket.broadcast.emit('newMessage',{
+			from: 'Admin',
+			text: `${params.name} has joined`,
+			createdAt: moment().valueOf()
+		})
+		callback();
 	})
 
-	socket.broadcast.emit('newMessage',{
-		from: 'Admin',
-		text: 'New user joined',
-		createdAt: moment().valueOf()
-	})
+	// socket.broadcast.emit('newMessage',{
+	// 	from: 'Admin',
+	// 	text: 'New user joined',
+	// 	createdAt: moment().valueOf()
+	// })
 
 	socket.on('createLocationMessage',(coords)=>{
 		io.emit('newLocationMessage',generateLocationMessage('Admin',`${coords.latitude},${coords.longitude}`))
